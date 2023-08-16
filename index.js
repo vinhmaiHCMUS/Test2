aaea26a20081d56d25a469fe69fa777234ab629d
Your Name <vinhmai2099@gmail.com>
2023-08-16 11:36:31 +0700

    Upload files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Day la do an cong diem cua 22424020 Mai Quang Vinh')
+    res.send('Hello World! Day la do an cong diem cua 22424020 Mai Quang Vinh lien thong dai hoc')
 })
 
 app.get('/products', (req, res) => {
